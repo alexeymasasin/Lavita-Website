92b49b3ea0b412d04f835176ecc6d334b7be7c5d
Alexey Masasin <alexeymasasin@gmail.com>
2023-09-16 09:44:49 +0300

    UPD

--- js/script.js
+++ js/script.js
@@ -1,3 +1,39 @@
 window.onload = function () {
   document.body.className += " loaded";
 };
+
+let scrollToTopBtn = document.getElementById("BtnScrollToTop");
+
+window.onscroll = function () {
+  if (
+    document.body.scrollTop > 200 ||
+    document.documentElement.scrollTop > 200
+  ) {
+    scrollToTopBtn.style.opacity = "1";
+  } else {
+    scrollToTopBtn.style.opacity = "0";
+  }
+};
+
+function scrollToTopFunc() {
+  document.body.scrollTop = 0; // Safari
+  document.documentElement.scrollTop = 0; // Chrome, Exp, Moz, Opera
+}
+
+document
+  .getElementById("signup-modal__box_close-btn")
+  .addEventListener("click", function () {
+    document.getElementById("signup-modal").classList.remove("open");
+  });
+
+document
+  .getElementById("courses-signup-btn")
+  .addEventListener("click", function () {
+    document.getElementById("signup-modal").classList.add("open");
+  });
+
+document
+  .getElementById("courses-signup_from_home-btn")
+  .addEventListener("click", function () {
+    document.getElementById("signup-modal").classList.add("open");
+  });
